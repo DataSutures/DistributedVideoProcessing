00f3662d6c88d0c49f8efce43bff1664429573a2
DataSutures <datasutures@gmail.com>
2018-10-04 17:43:54 -0500

    modified py script for pachyderm distributed fs

--- ParseVideosFrames.py
+++ ParseVideosFrames.py
@@ -1,25 +1,19 @@
+# frames.py
 import sys
-import argparse
 import os
 import cv2
-print(cv2.__version__)
 
-def extractImages(pathIn, pathOut):
+def extractFrames(vid):
     count = 0
-    vidcap = cv2.VideoCapture(pathIn)
-    success,image = vidcap.read()
+    vidcap = cv2.VideoCapture(vid)
+    success,frame = vidcap.read()
     success = True
     while success:
-      success,image = vidcap.read()
+      success,frame = vidcap.read()
       print ('Read a new frame: ', success)
-      cv2.imwrite(os.path.normpath(pathOut + "/frame%d.jpg" % count), image)
+      cv2.imwrite(os.path.normpath("/pfs/out" + "/frame%d.jpg" % count), frame)
       count = count + 1
 
-if __name__=="__main__":
-    print("aba")
-    a = argparse.ArgumentParser()
-    a.add_argument("--pathIn", help="path to video")
-    a.add_argument("--pathOut", help="path to images")
-    args = a.parse_args()
-    print(args)
-    extractImages(args.pathIn, args.pathOut)
+for dirpath, dirs, files in os.walk("/pfs/videos"):
+	for file in files:
+    extractFrames(os.path.join(dirpath,file))
