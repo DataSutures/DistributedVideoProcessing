fdfe9932bb252256bf34e7f1d1f628755f184a2e
DataSutures <datasutures@gmail.com>
2018-10-05 10:02:29 -0500

    test

--- ParseVideosFrames.py
+++ ParseVideosFrames.py
@@ -4,17 +4,18 @@ import os
 import cv2
 
 def extractFrames(vid):
-    tail = os.path.split(vid)[1]
-    count = 0
-    vidcap = cv2.VideoCapture(vid)
-    success,frame = vidcap.read()
-    success = True
-    while success:
-      success,frame = vidcap.read()
-      print ('Read a new frame: ', success)
-      cv2.imwrite(os.path.normpath("/pfs/out" + os.path.splitext(tail)[0]+"frame%d.jpg" % count), frame)
-      count = count + 1
-
+#    tail = os.path.split(vid)[1]
+     count = 0
+#    vidcap = cv2.VideoCapture(vid)
+#    success,frame = vidcap.read()
+#    success = True
+ #   while success:
+#      success,frame = vidcap.read()
+ #     print ('Read a new frame: ', success)
+  #    cv2.imwrite(os.path.normpath("/pfs/out" + os.path.splitext(tail)[0]+"frame%d.jpg" % #count), frame)
+    count = count + 1
+    h = open(“/pfs/out/hello.txt”, “w”) 
+	fh.write(“Put the text you want to add here”) 
 for dirpath, dirs, files in os.walk("/pfs/videos"):
 	for file in files:
     extractFrames(os.path.join(dirpath,file))
