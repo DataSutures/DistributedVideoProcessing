27fd3162d9dcd595c10e66ab9a025396e42b7d44
DataSutures <datasutures@gmail.com>
2018-10-05 18:39:37 -0500

    edit imsave path

--- ParseVideoFrames.py
+++ ParseVideoFrames.py
@@ -3,6 +3,7 @@ import sys
 import cv2
 
 def extractFrames(vid):
+    tail = os.path.split(vid)[1]
     count = 0
     vidcap = cv2.VideoCapture(vid)
     success, frame = vidcap.read()
@@ -10,7 +11,7 @@ def extractFrames(vid):
     while success:
         success, frame = vidcap.read()
         sys.stdout.write('Read a new frame: %s' % success)
-        cv2.imwrite(os.path.normpath("/pfs/frames" + "/frame%d.jpg " % count), frame)
+        cv2.imwrite(os.path.join("/pfs/out", os.path.splitext(tail)[0]+str(count)+'.jpg'), frame)
         count = count + 1
 
 
