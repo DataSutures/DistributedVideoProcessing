6d9902ca4fe31a2f55ae579870415991da6c3e43
DataSutures <datasutures@gmail.com>
2018-10-07 12:24:24 -0500

    latest py script

--- ParseVideoFrames.py
+++ ParseVideoFrames.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import cv2
-from matplotlib import pyplot as plt
 
 def extractFrames(vid):
+    tail = os.path.split(vid)[1]
+    count = 0
     vidcap = cv2.VideoCapture(vid)
     success, frame = vidcap.read()
-    if success:
+    success = True
+    while success:
         success, frame = vidcap.read()
-        tail = os.path.split(vid)[1]
-        sys.stdout.write('\nRead a new frame: %s' % success)
-        plt.imsave(os.path.join("/pfs/out", os.path.splitext(tail)[0]+'_'+str(count)+'.png'),frame, cmap = 'gray')
+        sys.stdout.write('Read a new frame: %s' % success)
+        cv2.imwrite(os.path.join("/pfs/out", os.path.splitext(tail)[0]+str(count)+'.png'), frame)
+        count = count + 1
 
 
-for dirpath, dirs, files in os.walk("/pfs/videos"):
+for dirpath, dirs, files in os.walk("/pfs/vids"):
     for file in files:
         extractFrames(os.path.join(dirpath, file))
