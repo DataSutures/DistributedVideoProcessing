36d91483edc6ef9c1de2a6377dc41edf459c10cf
DataSutures <datasutures@gmail.com>
2018-10-07 18:35:19 -0500

    lort halp me plez

--- ParseVideoFrames.py
+++ ParseVideoFrames.py
@@ -3,18 +3,16 @@ import sys
 import cv2
 
 def extractFrames(vid):
-    tail = os.path.split(vid)[1]
     count = 0
     vidcap = cv2.VideoCapture(vid)
     success, frame = vidcap.read()
-    success = True
     while success:
         success, frame = vidcap.read()
         sys.stdout.write('Read a new frame: %s' % success)
-        cv2.imwrite(os.path.join("/pfs/out", os.path.splitext(tail)[0]+str(count)+'.png'), frame)
+        cv2.imwrite(os.path.join('/pfs/out', '/'+str(count)+'.png'), frame)
         count = count + 1
 
 
-for dirpath, dirs, files in os.walk("/pfs/vids"):
+for dirpath, dirs, files in os.walk('/pfs/videos'):
     for file in files:
         extractFrames(os.path.join(dirpath, file))
