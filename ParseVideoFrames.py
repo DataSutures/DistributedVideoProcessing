851975b189b434177687fbec03167325dfe38c2a
DataSutures <datasutures@gmail.com>
2018-10-07 10:50:30 -0500

    yet another go at getting .py to output data

--- ParseVideoFrames.py
+++ ParseVideoFrames.py
@@ -2,12 +2,14 @@ import os
 import sys
 import cv2
 from matplotlib import pyplot as plt
+
 def extractFrames(vid):
     vidcap = cv2.VideoCapture(vid)
     success, frame = vidcap.read()
     if success:
         success, frame = vidcap.read()
-        sys.stdout.write('Read a new frame: %s' % success)
+        tail = os.path.split(vid)[1]
+        sys.stdout.write('\nRead a new frame: %s' % success)
         plt.imsave(os.path.join("/pfs/out", os.path.splitext(tail)[0]+'_'+str(count)+'.png'),frame, cmap = 'gray')
 
 
